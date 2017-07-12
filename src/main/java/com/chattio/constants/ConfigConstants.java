ca601ed751c473f974d8756ae0778320b923c681
Shabala Yevhen <Genya@MacBookPro-Shabala-3.local>
2017-07-13 01:25:39 +0300

    final

--- src/main/java/com/chattio/constants/ConfigConstants.java
+++ src/main/java/com/chattio/constants/ConfigConstants.java
@@ -4,5 +4,5 @@ public class ConfigConstants {
     public static final String RESOURCE_ID = "api";
     public static final int TOKEN_VALIDITY = 120000;
     public static final int REFRESH_TOKEN_VALIDITY = 600;
-    public static final String CORS_ORIGIN = "http://chattio-client.herokuapp.com";
+    public static final String CORS_ORIGIN = "http://localhost:4200";
 }
